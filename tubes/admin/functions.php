8ef67a23a38b55326fefc6ccffadd83cce2a8b2d
muhjam <muhhjam@gmail.com>
2022-05-29 15:10:01 +0700

    selesai tubes

--- tubes/admin/functions.php
+++ tubes/admin/functions.php
@@ -429,7 +429,7 @@ return mysqli_affected_rows($conn);
 function rupiah($harga){
 	global $conn;
 
-	$hasil_rupiah = "Rp " . number_format($harga,2,',','.');
+	$hasil_rupiah = "Rp. " . number_format($harga,2,',','.');
 	return $hasil_rupiah;
  
 
